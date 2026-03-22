985b30eaf692253aa10a66b44b5064ab40e56544
Debuntu <49510414+Debuntu@users.noreply.github.com>
2026-03-21 21:18:04 -0500

    allowing public access to eks cluster

--- main.tf
+++ main.tf
@@ -41,7 +41,6 @@ module "eks" {
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
-  cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = false
 
   cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
